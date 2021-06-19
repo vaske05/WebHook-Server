4ecbd2b11575292d0bcfe2f70792f996a32de335
Milan Vasic <milan.vasic@leanpay.com>
2021-06-19 23:28:08 +0200

    User conttroller interface

---
+++ src/main/java/com/elfak/whserver/web/rest/controller/UserControllerImpl.java
@@ -0,0 +1,15 @@
+package com.elfak.whserver.web.rest.controller;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.elfak.whserver.web.WebConstant;
+
+@RestController
+@RequestMapping(value = WebConstant.USER_BASE_URL, produces = APPLICATION_JSON_VALUE)
+public class UserControllerImpl {
+
+
+}
