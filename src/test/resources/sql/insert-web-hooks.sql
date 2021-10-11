ecad715a6704e421ef576e40b414ef6f7243b262
Milan Vasic <vaske494@gmail.com>
2021-10-11 23:56:57 +0200

    - More test data

--- src/test/resources/sql/insert-web-hooks.sql
+++ src/test/resources/sql/insert-web-hooks.sql
@@ -1,2 +1,3 @@
 insert into web_hook(id, name, url, type, user_id)
-VALUES ('0', 'Web hook 1', 'localhost:8082/api/receive-air-data', 'AIR_DATA', '0');
+VALUES ('0', 'Web hook 1', 'localhost:8082/api/receive-air-data', 'AIR_DATA', '0'),
+       ('1', 'Web hook 2', 'localhost:8082/api/receive-covid-data', 'COVID_DATA', '0');
