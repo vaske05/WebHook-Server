ca64cbe296ec1d6be8a1b7f147ba8732a831ffa2
Milan Vasic <vaske494@gmail.com>
2021-10-22 12:21:33 +0200

    - notnull import fix

--- src/main/java/com/elfak/whserver/repository/WebHookRepository.java
+++ src/main/java/com/elfak/whserver/repository/WebHookRepository.java
@@ -2,10 +2,10 @@ package com.elfak.whserver.repository;
 
 import com.elfak.whserver.enumeration.WebHookType;
 import com.elfak.whserver.model.WebHook;
-import org.jetbrains.annotations.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import javax.validation.constraints.NotNull;
 import java.util.List;
 import java.util.Optional;
 
