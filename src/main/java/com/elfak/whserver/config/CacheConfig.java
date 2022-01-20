95204e7b321490b8a1bdf1b41ddf31ba979fc1db
Milan Vasic <vaske494@gmail.com>
2022-01-21 00:44:29 +0100

    - Caching

---
+++ src/main/java/com/elfak/whserver/config/CacheConfig.java
@@ -0,0 +1,30 @@
+package com.elfak.whserver.config;
+
+import com.elfak.whserver.helpers.Constants;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+    @Bean
+    public Caffeine<Object, Object> caffeineConfig() {
+        return Caffeine.newBuilder()
+                .expireAfterWrite(60, TimeUnit.MINUTES);
+    }
+
+    @Bean
+    public CacheManager cacheManager(Caffeine<Object, Object> caffeine) {
+        CaffeineCacheManager caffeineCacheManager = new CaffeineCacheManager();
+        caffeineCacheManager.getCache(Constants.CacheNames.SELECT_DATA_CACHE);
+        caffeineCacheManager.setCaffeine(caffeine);
+        return caffeineCacheManager;
+    }
+}
---
+++ src/main/java/com/elfak/whserver/helpers/Constants.java
@@ -0,0 +1,7 @@
+package com.elfak.whserver.helpers;
+
+public class Constants {
+    public static final class CacheNames {
+        public static final String SELECT_DATA_CACHE = "select_data_cache";
+    }
+}
