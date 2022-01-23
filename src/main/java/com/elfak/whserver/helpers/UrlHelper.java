af558f0b321670b9c1c4d06e8bc26a7403b376d0
Milan Vasic <vaske494@gmail.com>
2022-01-24 00:13:34 +0100

    - UrlHelper method bug fix - UrlHelper unit test

--- src/main/java/com/elfak/whserver/helpers/UrlHelper.java
+++ src/main/java/com/elfak/whserver/helpers/UrlHelper.java
@@ -1,17 +1,12 @@
 package com.elfak.whserver.helpers;
 
-import lombok.NoArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@NoArgsConstructor
-@Service
 public class UrlHelper {
 
-    public String replaceStringOccurrence(String source, String oldString, String newString) {
+    public static String replaceStringOccurrence(String source, String oldString, String newString) {
         String desiredString = source;
         while (desiredString.contains(oldString)) {
-            String firstPart = source.substring(0, source.indexOf(oldString));
-            String secondPart = source.substring(source.indexOf(oldString) + 3);
+            String firstPart = desiredString.substring(0, desiredString.indexOf(oldString));
+            String secondPart = desiredString.substring(desiredString.indexOf(oldString) + 3);
             desiredString = firstPart + newString + secondPart;
         }
         return desiredString;
