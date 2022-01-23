9dc03ff10fec28626e876c9092c9a21f50528517
Milan Vasic <vaske494@gmail.com>
2022-01-24 00:25:33 +0100

    - Air vendor response classes

---
+++ src/main/java/com/elfak/whserver/model/dto/vendor/AirQualityRegionsResponseRaw.java
@@ -0,0 +1,27 @@
+package com.elfak.whserver.model.dto.vendor;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AirQualityRegionsResponseRaw {
+
+    private String status;
+    private List<Data> data;
+
+    @lombok.Data
+    @Builder
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class Data {
+        public String state;
+
+    }
+}
