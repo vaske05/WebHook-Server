66a04f412af3230f7e9a54147977c18e26038aa7
Milan Vasic <vaske494@gmail.com>
2022-01-24 15:29:58 +0100

    - New cache names

--- src/main/java/com/elfak/whserver/helpers/Constants.java
+++ src/main/java/com/elfak/whserver/helpers/Constants.java
@@ -2,6 +2,8 @@ package com.elfak.whserver.helpers;
 
 public class Constants {
     public static final class CacheNames {
-        public static final String SELECT_DATA_CACHE = "select_data_cache";
+
+        public static final String COVID_SELECT_INPUT_CACHE = "covid_select_input_cache";
+        public static final String AIR_SELECT_INPUTS_CACHE = "air_select_inputs_cache";
     }
 }
